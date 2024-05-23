384da966481718980ba5c5aba30fb1d77d6a78ab
MadhuraUppar <149491686+MadhuraUppar@users.noreply.github.com>
2024-05-23 19:34:51 +0530

    removed comments in script block

--- azure/azure_python_script.py
+++ azure/azure_python_script.py
@@ -36,26 +36,15 @@ def azure_users(azure_username, azure_password, azure_object_id):
                 $Credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
                 
                 try {
-                    # Attempt to connect to Azure account
                     $nomsg = Connect-AzAccount -Credential $Credentials -TenantId $TenantId -AuthScope 'https://graph.microsoft.com'
-                
-                    # Check if connection was successful
                     if ($nomsg) {
                         Write-Host "Connected to Azure account successfully."
-                        
-                        # Get access token for Microsoft Graph
                         $contextForMSGraphToken = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile.DefaultContext
                         $newBearerAccessTokenRequest = [Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.AuthenticationFactory.Authenticate($contextForMSGraphToken.Account, $contextForMSGraphToken.Environment, $TenantId, $null, [Microsoft.Azure.Commands.Common.Authentication.ShowDialog]::Never, $null, 'https://graph.microsoft.com')
-                        
-                        # Check if access token was obtained
                         if ($newBearerAccessTokenRequest) {
                             $AccessToken = $newBearerAccessTokenRequest.AccessToken
                             $SecureAccessToken = ConvertTo-SecureString -String $AccessToken -AsPlainText -Force
-                            
-                            # Connect to Microsoft Graph
                             Connect-MgGraph -AccessToken $SecureAccessToken -NoWelcome
-                            
-                            # Retrieve group members
                             Get-MgGroupMember -GroupId {azure_object_id} -all | ForEach-Object {
                                 Get-MgUser -UserId $_.Id
                             } | Select-Object DisplayName, UserPrincipalName, GivenName, Surname, Department, JobTitle   
