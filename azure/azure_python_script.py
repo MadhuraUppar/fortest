5e011100ceff7f25fb5d2474395c092ad41c29bf
MadhuraUppar <149491686+MadhuraUppar@users.noreply.github.com>
2024-05-23 17:32:36 +0530

    Update azure_python_script.py

--- azure/azure_python_script.py
+++ azure/azure_python_script.py
@@ -9,9 +9,10 @@ def azure_users(azure_username, azure_password, azure_object_id):
         # PowerShell script block to connect to Azure and fetch user details
         script_block = f""" 
             $User = '{azure_username}'
+            $TenantId = "0101c982-42b5-4932-8d76-28b81fc704c8"
             $PWord = ConvertTo-SecureString -String '{azure_password}' -AsPlainText -Force
             $Credentials = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
-            $nomsg=Connect-AzAccount -Credential $Credentials -TenantId '0101c982-42b5-4932-8d76-28b81fc704c8'
+            $nomsg=Connect-AzAccount -Credential $Credentials -TenantId $TenantId -AuthScope 'https://graph.microsoft.com'
             $contextForMSGraphToken = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile.DefaultContext
             $newBearerAccessTokenRequest = [Microsoft.Azure.Commands.Common.Authentication.AzureSession]::Instance.AuthenticationFactory.Authenticate($contextForMSGraphToken.Account, $contextForMSGraphToken.Environment, $contextForMSGraphToken.Tenant.id.ToString(), $null, [Microsoft.Azure.Commands.Common.Authentication.ShowDialog]::Never, $null, 'https://graph.microsoft.com')
             $AccessToken = $newBearerAccessTokenRequest.AccessToken
@@ -47,7 +48,7 @@ def azure_users(azure_username, azure_password, azure_object_id):
             print(output)
             print(result)
             # output format containing escape characters like '\x1b[32;1m' to remove this used re.compile
-            '''ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+            ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
             output = ansi_escape.sub('', output)
             lines = [
                 line.strip() for line in output.strip().split("\n") if line.strip()
@@ -66,9 +67,9 @@ def azure_users(azure_username, azure_password, azure_object_id):
                 data.setdefault("LastName", []).append(lastname)
                 data.setdefault("Department", []).append(department)
                 data.setdefault("JobTitle", []).append(job_title)
-            df = pd.DataFrame(data)'''
+            df = pd.DataFrame(data)
         
-        return 
+        return df
 
     except subprocess.CalledProcessError as e:
         print("Error connecting to Azure AD:", e)
@@ -82,5 +83,6 @@ def azure_users(azure_username, azure_password, azure_object_id):
 if __name__ == "__main__":
     # Fetch Azure users and insert into Snowflake
     print(sys.argv[2], sys.argv[3], sys.argv[4])
-    azure_users(sys.argv[2], sys.argv[3], sys.argv[4])
+    df = azure_users(sys.argv[2], sys.argv[3], sys.argv[4])
+    print(df)
     
